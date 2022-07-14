5649b340691dd23badb6d904b52baf3a5c9de64c
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-07-14 18:50:17 +0200

    Change AttributeHelpers.HasAttribute to use IsDefined

--- Runtime/Helpers/AttributeHelpers.cs
+++ Runtime/Helpers/AttributeHelpers.cs
@@ -1,28 +1,13 @@
 ﻿using System;
 using System.Reflection;
-using Sirenix.Utilities;
 
 namespace Helpers
 {
     public static class AttributeHelpers
     {
-        public static bool HasAttribute<T>(this ICustomAttributeProvider member) where T : Attribute
+        public static bool HasAttribute<T>(this ICustomAttributeProvider member, bool inherit = true) where T : Attribute
         {
-            return member.HasAttribute(typeof(T));
-        }
-
-        public static bool HasAttribute(this ICustomAttributeProvider member, Type type)
-        {
-            // TODO optimize
-            foreach (var attribute in member.GetAttributes())
-            {
-                if (attribute.GetType() == type)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return member.IsDefined(typeof(T), inherit);
         }
     }
 }
